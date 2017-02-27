dd33ec9f0009688f711205a38ae533d825aedfc0
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-02-27 00:07:18 -0500

    provided better messaging, new logout route for user, changes access token

--- bin/extras/users/private-routes.js
+++ bin/extras/users/private-routes.js
@@ -14,6 +14,7 @@ module.exports = function(model, app, express, models) {
 
 		var status;
 		var err = null;
+		var message = null;
 		var data = {};
 
 		//safely load our access token from headers
@@ -23,8 +24,9 @@ module.exports = function(model, app, express, models) {
 		if (!accessToken) {
 			status = 401;
 			err = 'Could not find valid access token header on request';
+			message = null;
 			data = null;
-			return res.status(status).send({ status, err, data });
+			return res.status(status).send({ status, err, message, data });
 		}
 
 		//once we find an access token, we still have to authenticate it against our model
@@ -34,8 +36,9 @@ module.exports = function(model, app, express, models) {
 		if (!user) {
 			status = 404;
 			err = 'Could not find any users with that access token.';
+			message = null;
 			data = null;
-			return res.status(status).send({ status, err, data });
+			return res.status(status).send({ status, err, message, data });
 		}
 
 		//finally, we've decided to allow further requests. we save our user and keep the req going
@@ -55,8 +58,9 @@ module.exports = function(model, app, express, models) {
 			//at this point, we merely return our auth'd user info
 			var status = 200;
 			var err = null;
+			var message = null;
 			var data = account.omitSensitive(req.user);
-			res.status(status).send({ status, err, data });
+			res.status(status).send({ status, err, message, data });
 		})
 
 
@@ -65,15 +69,19 @@ module.exports = function(model, app, express, models) {
 		*/
 
 		.put(function(req, res, next) {
-
-			//getting query from our auth'd user
-			var where = { id: req.user.id };
+			var status = 200;
+			var err = null;
+			var message = 'Saved your information.';
+			var data = {};
 
 			//checking to see if we have password in our payload
 			if (req.body.password) {
 
 				//if so, let's hash our password and set it to body
 				req.body.password = account.hashPassword(req.body.password);
+
+				//alert our user
+				message += ' Also, your password was changed.';
 
 				//since we're changing our password, we trigger an email
 				if (models.emails) {
@@ -86,11 +94,28 @@ module.exports = function(model, app, express, models) {
 			}
 
 			//making our update
-			model.updateWhere(where, req.body);
+			model.updateWhere({ id: req.user.id }, req.body);
+
+			//returning our response
+			return res.status(status).send({ status, err, message, data });
+
+		});
+
+	router.route('/logout')
+		.get(function(req, res, next) {
 			var status = 200;
 			var err = null;
+			var message = 'You are now logged out.';
 			var data = {};
-			return res.status(status).send({ status, err, data });
+
+			//we change our access token
+			var accessToken = account.generateHash();
+
+			//save our user with the new access token
+			model.updateWhere({ id: req.user.id }, { accessToken });
+
+			//returning our response
+			return res.status(status).send({ status, err, message, data });
 
 		});
 
