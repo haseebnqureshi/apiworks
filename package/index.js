e2b1a38ee43d69fd6a51680d5c122b266f9d04db
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-04-30 20:46:49 -0400

    ensuring we load .env from project filepath

--- package/index.js
+++ package/index.js
@@ -6,71 +6,78 @@ var packageJson = require('../package.json');
 
 var log = utils.log;
 
-/* INFO */
+module.exports = function(appDirname) {
 
-log('green', '| Welcome to your Apiworks application!', true);
-log('yellow', '| Version ' + packageJson.version, true);
-log('yellow', '| ' + packageJson.license + ' Licensed', true);
-log('gray', '| ', true);
-log('gray', '| It\'s very important to note what Apiworks is doing under the hood.', true);
-log('gray', '| It\'s a structured Express application that reads your application', true);
-log('gray', '| folder directory, looking for routes, middlewares, and render files.', true);
-log('gray', '| ', true);
-log('gray', '| The idea here, is whether you\'re crafting an API or an all-inclusive', true);
-log('gray', '| web app using Pug/Jade templates, you can clearly do that quickly and', true);
-log('gray', '| with incredible readability.', true);
-log('gray', '| ', true);
-log('cyan', '| Happy Coding!', true);
-log('gray', '| Haseeb Qureshi, Apiworks Author', true);
-log('gray', '| twitter.com/_hq, github.com/haseebnqureshi', true);
-log('gray', '| ', true);
-log('green', '| Now loading your application...', true);
-log('gray', '', true);
+	/* INFO */
 
-/* CONFIG */
+	log('green', '| Welcome to your Apiworks application!', true);
+	log('yellow', '| Version ' + packageJson.version, true);
+	log('yellow', '| ' + packageJson.license + ' Licensed', true);
+	log('gray', '| ', true);
+	log('gray', '| It\'s very important to note what Apiworks is doing under the hood.', true);
+	log('gray', '| It\'s a structured Express application that reads your application', true);
+	log('gray', '| folder directory, looking for routes, middlewares, and render files.', true);
+	log('gray', '| ', true);
+	log('gray', '| The idea here, is whether you\'re crafting an API or an all-inclusive', true);
+	log('gray', '| web app using Pug/Jade templates, you can clearly do that quickly and', true);
+	log('gray', '| with incredible readability.', true);
+	log('gray', '| ', true);
+	log('cyan', '| Happy Coding!', true);
+	log('gray', '| Haseeb Qureshi, Apiworks Author', true);
+	log('gray', '| twitter.com/_hq, github.com/haseebnqureshi', true);
+	log('gray', '| ', true);
+	log('green', '| Now loading your application...', true);
+	log('gray', '', true);
 
-log('yellow', 'Loading config via dotenv...');
+	/* CONFIG */
 
-require('dotenv').config();
+	var dotenvPath = appDirname + '/.env';
 
-log('green', '...Done!');
+	log('yellow', 'Loading config via dotenv...');
 
+	log('gray', dotenvPath);
 
-/* APIWORKS ENGINE */
+	require('dotenv').config({ path: dotenvPath });
 
-log('yellow', 'Loading apiworks engine...');
-
-var _ = require('underscore');
-
-var express = require('express');
-
-var read = require('fs-readdir-recursive');
-
-var bodyParser = require('body-parser');
-
-var pug = require('pug');
-
-var app = express();
-
-log('green', '...Done!');
+	log('green', '...Done!');
 
 
-/* INSTANTIATING API */
+	/* APIWORKS ENGINE */
 
-log('yellow', 'Instantiating application...');
+	log('yellow', 'Loading apiworks engine...');
+
+	var _ = require('underscore');
+
+	var express = require('express');
+
+	var read = require('fs-readdir-recursive');
+
+	var bodyParser = require('body-parser');
+
+	var pug = require('pug');
+
+	var app = express();
+
+	log('green', '...Done!');
+
+
+	/* INSTANTIATING API */
+
+	log('yellow', 'Instantiating application...');
 
 
 
-log('green', '...Done!');
+	log('green', '...Done!');
 
 
-/* STARTING API */
+	/* STARTING API */
 
-log('yellow', 'Starting application on port ' + (process.env.EXPRESS_PORT || 3000) + '...');
+	log('yellow', 'Starting application on port ' + (process.env.EXPRESS_PORT || 3000) + '...');
 
-app.listen(process.env.EXPRESS_PORT || 3000, function() {
+	app.listen(process.env.EXPRESS_PORT || 3000, function() {
 
-	log('green', `...Done!`);
+		log('green', `...Done!`);
 
-});
+	});
 
+};
