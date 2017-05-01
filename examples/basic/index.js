d1bdecc591fd91787dc3457e1fd336565dda497b
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-04-30 21:11:56 -0400

    basic example directory for todo app

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,4 +1,19 @@
 'use strict';
 
-require('../..')(__dirname);
-
+require('../..')(__dirname, {
+	folders: {
+		db: 'db',
+		models: 'models',
+		routes: 'routes',
+		static: 'static',
+		routeParamPrefix: '-'
+	},
+	files: {
+		get: '__get.js',
+		post: '__post.js',
+		put: '__put.js',
+		delete: '__delete.js',
+		middleware: '_middleware.js',
+		render: 'render.pug'
+	}
+});
