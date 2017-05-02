c8fd129f7526adb5e09721b6818d9bc1026ea147
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-05-02 12:42:31 -0400

    schema now works with pg package’s connection

--- examples/basic/db/postgres/schema.js
+++ examples/basic/db/postgres/schema.js
@@ -2,38 +2,44 @@
 
 var connect = require('./connect.js');
 
+var knex = require('knex')({ client: 'pg' });
+
 module.exports = function(log) {
 
 	connect(function(client) {
 		
+		var lists = knex.schema.createTableIfNotExists('lists', function(table) {
+			table.bigIncrements('list_id');
+			table.string('list_name');
+			table.timestamps();
+		}).toString();
 
+		var items = knex.schema.createTableIfNotExists('items', function(table) {
+			table.bigIncrements('item_id');
+			table.string('item_name');
+			table.bigInteger('list_id').unsigned();
+			table.foreign('list_id').references('lists.list_id')
+			table.timestamps();
+		}).toString();
 
+		var text = [lists, items].join("; ");
 
-		// knex.schema.createTableIfNotExists('lists', function(table) {
-		// 	table.bigIncrements('list_id');
-		// 	table.string('list_name');
-		// 	table.timestamps();
-		
-		// }).then(function(result) {
-		// 	return knex.schema.createTableIfNotExists('items', function(table) {
-		// 		table.bigIncrements('item_id');
-		// 		table.string('item_name');
-		// 		table.bigInteger('list_id').unsigned();
-		// 		table.foreign('list_id').references('lists.list_id')
-		// 		table.timestamps();
-		// 	});
-		// }).then(function() {
+		client.query(text, function(err, result) {
 
+			if (err) {
 
-		// }).catch(function(err) {
+				if (err.toString().match('constraint')) { return; }
+
+				log('red', err);
+
+				return;
+
+			}
+
+			log('green', 'Database schema created!');
+
+		});
 	
-		// 	if (err.toString().match('constraint')) { return; }
-			
-		// 	log('red', err);
-		
-		// });
-
-
 	});
 
 };
