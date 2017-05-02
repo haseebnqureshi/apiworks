424a5d5fc75496d8a66971080d7ad69f6a13b38d
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2017-05-01 20:33:37 -0400

    removed test db connection logic

--- examples/basic/routes/_middleware.js
+++ examples/basic/routes/_middleware.js
@@ -4,12 +4,6 @@ module.exports = function(db, models, log) {
 
 	return function(req, res, next) {
 
-
-		db.connect(function(client) {
-			console.log(client.toString());
-		});
-
-
 		log('cyan', '-- Middleware (home) --');
 
 		next();
